e1c534ab4a00c95fc7b4fdca5b5abba76df59f62
David Sorrentino <me@davidsorrentino.com>
2025-02-08 17:07:40 +0100

    Add unicorn image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,43 @@
+import Image from 'next/image';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Piccerella mia</h1>
+      <header id={styles.header}>
+        <Image src="/unicorn.png" alt="Unicorn" width={30} height={30} />
+        <h1>Piccerella mia</h1>
+      </header>
 
-      <p>
-        Piccerella mia, nata &apos;a matina,<br/>
-        cull&apos;oro &apos;int&apos; all&apos;uocchie e &apos;a faccia fina.<br/>
-        T&apos;avimmo cercata tant&apos;, cu&apos; pacienza e core,<br/>
-        t&apos;avimmo cercata comm&apos; &apos;e sera se cerca &apos;o sole.
-      </p>
+      <main>
+        <p>
+          Piccerella mia, nata &apos;a matina,<br/>
+          cull&apos;oro &apos;int&apos; all&apos;uocchie e &apos;a faccia fina.<br/>
+          T&apos;avimmo cercata tant&apos;, cu&apos; pacienza e core,<br/>
+          t&apos;avimmo cercata comm&apos; &apos;e sera se cerca &apos;o sole.
+        </p>
 
-      <p>
-        Si&apos; &apos;nu sciore ca nasce &apos;ngopp &apos;a neve,<br/>
-        ca sfida &apos;o friddo e cresce lieve lieve,<br/>
-        &apos;nu sciore ca sboccia senza rummore,<br/>
-        cu&apos; tutta &apos;a forza &apos;e &apos;nu grand&apos;ammore.
-      </p>
+        <p>
+          Si&apos; &apos;nu sciore ca nasce &apos;ngopp &apos;a neve,<br/>
+          ca sfida &apos;o friddo e cresce lieve lieve,<br/>
+          &apos;nu sciore ca sboccia senza rummore,<br/>
+          cu&apos; tutta &apos;a forza &apos;e &apos;nu grand&apos;ammore.
+        </p>
 
-      <p>
-        Si&apos; respiro &apos;e seta e stella &apos;ncielo,<br/>
-        carezza doce &apos;e &apos;nu pensiero,<br/>
-        si&apos; speranza ca s&apos;affaccia &apos;a vita,<br/>
-        &apos;nu segreto &apos;e luce infinita.
-      </p>
+        <p>
+          Si&apos; respiro &apos;e seta e stella &apos;ncielo,<br/>
+          carezza doce &apos;e &apos;nu pensiero,<br/>
+          si&apos; speranza ca s&apos;affaccia &apos;a vita,<br/>
+          &apos;nu segreto &apos;e luce infinita.
+        </p>
 
-      <p>
-        E mò t&apos;adduorme &apos;int&apos; &apos;a sta carezza,<br/>
-        &apos;nu suonno d&apos;oro, luntano &apos;o friddo,<br/>
-        e &apos;o core canta senza stanchezza:<br/>
-        Bianca mia bella, tesoro &apos;e &apos;stu munno.
-      </p>
+        <p>
+          E mò t&apos;adduorme &apos;int&apos; &apos;a sta carezza,<br/>
+          &apos;nu suonno d&apos;oro, luntano &apos;o friddo,<br/>
+          e &apos;o core canta senza stanchezza:<br/>
+          Bianca mia bella, tesoro &apos;e &apos;stu munno.
+        </p>
+      </main>
     </div>
   );
 }
